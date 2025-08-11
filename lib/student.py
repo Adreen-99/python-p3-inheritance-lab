6df47ecb8c52c7131ce46662e9f51a06cf9dcd2e
+ <+githinjiadreen27@gmail.com>
2025-08-11 14:47:15 +0300

    Inheritance OOP

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,16 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self,info):
+        self.knowledge.append(info)
+
+s = Student("Ada", "Lovelace")
+s.learn("Python basics")
+s.learn("Data structures")
+print(s.knowledge)  
+# ['Python basics', 'Data structures']
--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,20 @@ import random
 
 class Teacher(User):
 
-    def teach(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = [
+            "Python basics",
+            "Object-oriented programming",
+            "Git version control"
+        ]
+
+    def teach(self): 
+        if not self.knowledge:  
+            return None
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[random_index]  
+    
+teacher = Teacher("My", "Teacher")
+teacher.knowledge.extend(["Python", "OOP", "Inheritance"])
+print(teacher.teach())  
